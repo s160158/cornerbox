4887cc9d76fef6fb5e042277dc0aa26c88a0e182
s160158 <s160158@student.dtu.dk>
2017-11-20 14:58:40 +0100

    Code refactoring. Redefined purpose

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
 # What does it do?
 
-Generates two types of data used for testing meshes in MIKE
+Extract a box from a raster file (.tif) and output boundary XYZ-file for mesh generation in MIKE. Compatible XYZ-file scatter data are also generated for mesh interpolation.
 
 ## MIKE boundary file
 
 Extract a box by cornerpoint from a .tif raster image + output a .xyz-file of cornerpoints. The generated .xyz-file has the following form
 
 722600 6184300 0.0 1 0 0
+
 722600 6184550 0.0 0 0 0
+
 722600 6184550 0.0 1 0 0
+
 722850 6184550 0.0 0 0 0
+
 722850 6184550 0.0 1 0 0
+
 722850 6184300 0.0 0 0 0
+
 722850 6184300 0.0 1 0 0
+
 722600 6184300 0.0 0 0 0
 
 This creates 4 arcs. One for each side of a rectangle.
@@ -21,7 +28,13 @@ This creates 4 arcs. One for each side of a rectangle.
 
 
 # Todo
+restate purpose.. more singular
+
+NO RESAMPLING.. its done in MIKE by interpolation
+
 
 * Connectivity for nodes/vertices. What value
 * Two ways: (1) separation into callable functions (2) main() function that generates all the needed files, i.e., .tif (full + reduced), .xyz (of reduced) and a .xyz of arcs connecting cornerpoints to be imported in MIKE
 * Proper python notation
+* Make a json(flat text, possibly without json) config file (tool paths, etc.) 
+* GDAL instead of precompiled function
--- cornerbox.py
+++ cornerbox.py
@@ -1,112 +1,199 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os #filesystem
-import re #regular expression
-import gdal
+import os  # filesystem
 import subprocess
-import math # (sqrt, )
+import math  # sqrt,
+
+
+def create_folder_structure(cornerlist):
+    """
+    Generates the used folder structure
+    :param cornerlist:
+    :return: None
+    """
+    foldername = './{}/{}'.format('extract', folder_name(cornerlist))
+
+    if not os.path.exists(foldername):
+        print 'created folder: {}'.format(foldername)
+        os.mkdir(foldername)
+        os.mkdir(foldername + '/o')  # Original (resolution equal to the original raster)
+        os.mkdir(foldername + '/r')  # Resample (resolution different... )
+        os.mkdir(foldername + '/s')  # Shape (containing shapefiles marking extent of extract)
+        os.mkdir(foldername + '/m')  # Mesh (MIKE compatible mesh files)
 
 
 def corners(ulx, uly, lx, ly):
+    """
+    Corner points
+    :param ulx: Upper-left x-coordinate
+    :param uly: Upper-left y-coordinate
+    :param lx: length along x (positive: east)
+    :param ly: length along y (positive: north)
+    :return: List of corner coordinates
+    """
     return [ulx, uly, ulx + lx, uly + ly]
 
+
 def box_xyz(cornerlist):
-    fopen = open('./xyz/test.xyz', 'a')
+    """
+    MIKE compatible .xyz shapefile marking extent of extract
+    :param cornerlist: List of corner coordinates
+    :return: None
+    """
+    length = get_lengths(cornerlist)
+    fout = './extract/{}/s/box_{}x{}.xyz'.format(folder_name(cornerlist), num2str(length[0], 'length'), \
+                                                 num2str(length[1], 'length'))
+    fopen = open(fout, 'w')
 
     z = '0.0'
 
-    line = ''.join([str(cornerlist[0]), ' ', str(cornerlist[1]), ' ', z, ' 1 0 0\n']) # first point 1 (node)
+    # Make MIKE arcs. 1 - nodes, 0 - vertices
+    line = ''.join([str(cornerlist[0]), ' ', str(cornerlist[1]), ' ', z, ' 1 0 0\n'])  # (ulx, uly)
     fopen.write(line)
-    line = ''.join([str(cornerlist[0]), ' ', str(cornerlist[3]), ' ', z, ' 0 0 0\n']) # following 0 points are vertices in the arc
+    line = ''.join([str(cornerlist[0]), ' ', str(cornerlist[3]), ' ', z, ' 0 0 0\n'])  # (ulx, lry)
     fopen.write(line)
-    line = ''.join([str(cornerlist[0]), ' ', str(cornerlist[3]), ' ', z, ' 1 0 0\n']) # new arc (point 1)
+    line = ''.join([str(cornerlist[0]), ' ', str(cornerlist[3]), ' ', z, ' 1 0 0\n'])  # (ulx, lry) # End and beginning
     fopen.write(line)
-    line = ''.join([str(cornerlist[2]), ' ', str(cornerlist[3]), ' ', z, ' 0 0 0\n'])
+    line = ''.join([str(cornerlist[2]), ' ', str(cornerlist[3]), ' ', z, ' 0 0 0\n'])  # (lrx, lry)
     fopen.write(line)
-    line = ''.join([str(cornerlist[2]), ' ', str(cornerlist[3]), ' ', z, ' 1 0 0\n'])
+    line = ''.join([str(cornerlist[2]), ' ', str(cornerlist[3]), ' ', z, ' 1 0 0\n'])  # (lrx, lry)
     fopen.write(line)
-    line = ''.join([str(cornerlist[2]), ' ', str(cornerlist[1]), ' ', z, ' 0 0 0\n'])
+    line = ''.join([str(cornerlist[2]), ' ', str(cornerlist[1]), ' ', z, ' 0 0 0\n'])  # (lrx, uly)
     fopen.write(line)
-    line = ''.join([str(cornerlist[2]), ' ', str(cornerlist[1]), ' ', z, ' 1 0 0\n'])
+    line = ''.join([str(cornerlist[2]), ' ', str(cornerlist[1]), ' ', z, ' 1 0 0\n'])  # (lrx, uly)
     fopen.write(line)
-    line = ''.join([str(cornerlist[0]), ' ', str(cornerlist[1]), ' ', z, ' 0 0 0\n'])
+    line = ''.join([str(cornerlist[0]), ' ', str(cornerlist[1]), ' ', z, ' 0 0 0\n'])  # (ulx, uly) # Closes loop
     fopen.write(line)
 
     fopen.close()
 
 
-def square_length(npixels, res):
-    return int(math.sqrt(npixels) * res)
+def box_shp(cornerlist):
+    # ESRI .shp shapefile marking extent of extract
+    length = get_lengths(cornerlist)
+    fout = './extract/{}/s/box_{}x{}.shp'.format(folder_name(cornerlist), num2str(length[0], 'length'), \
+                                                 num2str(length[1], 'length'))
+    fopen = open(fout, 'w')
+
+    z = '0.0'
+
+    fopen.close()
+
+
+def get_lengths(cornerlist):
+    """
+    Side length of box
+    :param cornerlist: List of corner coordinates
+    :return: Pair of box side lengths
+    """
+    return abs(cornerlist[0] - cornerlist[2]), abs(cornerlist[1] - cornerlist[3])
+
 
 def num2str(num, type='resolution'):
-    '''
-    convert a number to a string with zero-padding. Used for informative naming of files
-    :param num: a float
-    :return: zero-padded multiplied by 10
-    '''
-    if (type == 'resolution'):
+    """
+    Convert a number to a string with zero-padding. Used for informative naming of files
+    :param num: Any number (float or integer)
+    :param type: 'resolution'(default) or 'length'
+    :return: Zero-padded number
+    """
+    #
+    if type == 'resolution':
         round(num, 2)
         num = num * 10
-        num = int(num) # convert to integer
+        num = int(num)
         num = '{:03d}'.format(num)
-    elif (type == 'length'):
+    elif type == 'length':
         num = int(num)
         num = '{:04d}'.format(num)
+    else:
+        return 1  # throw error!
 
     return num
 
 
-def box_tif(tif, tif_extract, cornerlist, npixels, res): #tif_extract is a basename
+def folder_name(cornerlist):
+    """
+    Folder name for box (saving the output files into)
+    :param cornerlist: List of corner coordinates
+    :return: A string
+    """
+    # Folder name to store output file. An extract is referred to by the (ulx, uly) corner point.
+    return 'box{}_{}'.format(str(cornerlist[0]), str(cornerlist[1]))  # Folder name "box_(ulx)_(uly)"
+
+
+def box_tif(tif, cornerlist):
+    """
+    Extract a box from a larger raster file
+    :param tif: Large raster file to extract box from
+    :param cornerlist: List of corner coordinates
+    :return: None (Generates the files)
+    """
     # define path to the used tool (installed through OSGEO4W)
     tool = 'gdal_translate.exe'
     path_tool = 'C:/OSGeo4W64/bin/%s' % tool
 
-    l = square_length(npixels, res) #square side length
+    length = get_lengths(cornerlist)
 
-    #tif_out = './t/resolution_full/test_%sx%s.tif' % (num2str(l, 'length'), num2str(l, 'length'))
+    print "side lengths: " + str(length)
+
     print 'extracting square with corner points: (ulx, uly, lrx, lry) = (%d, %d, %d, %d)' % (cornerlist[0], cornerlist[1], cornerlist[2], cornerlist[3])
     print tif
-    tif_extract = tif_extract.join(['_%sx%s_r04x04.tif' % (num2str(l, 'length'), num2str(l, 'length'))])
-    print tif_extract
+
+
+    tif_extract = './extract/{}/o/box_{}x{}_r04x04.tif'.format(folder_name(cornerlist), num2str(length[0], 'length'), num2str(length[1], 'length')) #find the resolution! not always 40cm
+    print 'saving to :' + tif_extract
 
     try:
         subprocess.call('%s -projwin %s %s %s %s %s %s' % (path_tool, cornerlist[0], cornerlist[1], cornerlist[2], cornerlist[3], tif, tif_extract))
     except WindowsError:
-        print "Could not find tool!"
+        print 'Could not find tool! ({})'.format(path_tool)
         return 1
 
-    #resample file so that it has a maximumum number of pixels given by max_pixels
-    resample(tif_extract, './resolution_reduced/test_%sx%s_r%sx%s.tif' % (num2str(l, 'length'), num2str(l, 'length'), num2str(res), num2str(res)), [res])
-    return 0
+    tif2xyz(tif_extract)
 
-def resample(tif, tif_reduced, cornerlist):
+
+def resample(tif, tif_reduced, reslist):
+    # Resample a tif at different resolution
     tool = 'gdalwarp.exe'
     path_tool = 'C:/OSGeo4W64/bin/%s' % tool
 
 
-    for res in cornerlist:
+    for res in reslist:
         try:
             subprocess.call('%s -tr %s %s %s %s -overwrite' % (path_tool, res, res, tif, tif_reduced))
         except WindowsError:
-            print "Could not find tool!"
+            print 'Could not find tool! ({})'.format(path_tool)
             return 1
-        tif2xyz( '.' + tif_reduced.split('.')[1])
+        tif2xyz(tif_reduced)  # Filename doesn't change with res
+
 
 def tif2xyz(tif):
+    """
+    Convert .tif to .xyz
+    :param tif: Raster file (.tif) to convert to XYZ-file
+    :return: None
+    """
     tool = 'gdal_translate.exe'
     path_tool = 'C:/OSGeo4W64/bin/%s' % tool
 
+    tif = '.' + tif.split('.')[1]  # Remove extension
     try:
         subprocess.call('%s -of XYZ %s.tif %s.xyz' % (path_tool, tif, tif))
     except WindowsError:
-        print "Could not find tool!"
+        print 'Could not find tool! ({})'.format(path_tool)
         return 1
 
+
 if __name__=='__main__':
-    cornerlist = corners(722600, 6184300, 250, -250)
+    cornerlist = corners(722600, 6184300, 280, -280) # For small 0.4x0.4 m resolution area
+    #cornerlist = corners(722600, 6184300, 560, -560) # For larger resolution than 0.8x0.8 m
+    create_folder_structure(cornerlist)
+
     box_xyz(cornerlist)
 
+    # .tif file to extract from:
     tif = "../01_DTM/DHYMRAIN.tif"
-    tif_extract = "./resolution_full/test"
-    box_tif(tif, tif_extract, cornerlist, 100000, 1.6)
+    box_tif(tif, cornerlist)
+
+    #print rectangle_lengths(500000, 2, 0.8, 0.4)
